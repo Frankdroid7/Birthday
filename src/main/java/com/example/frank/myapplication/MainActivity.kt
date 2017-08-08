9e83cdd1dd29faf3f9d848f04e78a60ea2b505c8
Frankotech <30746679+Frankotech@users.noreply.github.com>
2017-08-08 23:08:04 +0100

    Add files via upload
    Birthday Card

---
+++ src/main/java/com/example/frank/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.frank.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
